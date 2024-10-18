46c0a8bd4953ee34b30edaa06a5e2593b2c39b53
pontacard <ryotatsumiryo288@gmail.com>
2024-10-19 01:00:55 +0900

    SOT_bif_paper

--- spherical/Bifrucation_diagram/SOT_bif.jl
+++ spherical/Bifrucation_diagram/SOT_bif.jl
@@ -71,7 +71,7 @@ BK = [0.0, 200.0, 0.0]
 Bac_phase = [0.0, 0.0, 0.0]
 α = 0.05
 γ = 0.176335977
-ω = 20.232
+ω = 20.2
 dt = 0.001
 spin = Lya.para(dt,α,B, BK, γ, ω, Bac_phase, LLG!)
 
@@ -79,5 +79,5 @@ spin = Lya.para(dt,α,B, BK, γ, ω, Bac_phase, LLG!)
 #println(length(his))
 #println(Lya.poincare(spin, his, "x", 700, 790))
 #Lya.Si_Sj_phase(2,1,700,750)
-Bac_eval = Vector(10:0.1:35)
+Bac_eval = Vector(10:0.025:35)
 SOT_Bifrucation_map(spin, "y", tspan, S0, Bac_eval, 788000, 799900)
